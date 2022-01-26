c4a3d18c96a1a35740fd962a2cf4caa2bc34f6db
yosmerymendez22@hotmail.com <40107868+Edixon-vanVliet@users.noreply.github.com>
2022-01-26 12:51:38 -0400

    Agregar DepartamentoModal

---
+++ ClientApp/src/components/departamentos/DepartamentosModal.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+
+export const DepartamentoModal = ({
+  title,
+  show,
+  onClose,
+  onChange,
+  departamento,
+  onSave,
+}) => {
+  return (
+    <Modal show={show} onHide={() => onClose()}>
+      <Modal.Header closeButton>
+        <Modal.Title>{title}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Nombre"
+              name="nombre"
+              value={departamento.nombre}
+              onChange={onChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Descripción</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Descripción"
+              name="descripcion"
+              value={departamento.descripcion}
+              onChange={onChange}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={() => onClose()}>
+          Cerrar
+        </Button>
+        <Button variant="primary" onClick={() => onSave()}>
+          Guardar Cambios
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
