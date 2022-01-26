f293839985d3f45be1a765c19b2078d707aec51c
yosmerymendez22@hotmail.com <40107868+Edixon-vanVliet@users.noreply.github.com>
2022-01-26 10:55:22 -0400

    Agregar Models de Departamento

---
+++ Models/Departamento.cs
@@ -0,0 +1,8 @@
+namespace BleylyMendez.Models;
+
+public class Departamento
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = "";
+    public string Descripcion { get; set; } = "";
+}
