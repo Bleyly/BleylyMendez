bda91aa14420e2062d0f86410c33d72a0e335bcd
yosmerymendez22@hotmail.com <40107868+Edixon-vanVliet@users.noreply.github.com>
2022-01-26 14:58:37 -0400

    Correcion de nombre y actualizacion de migracion

---
+++ Data/Migrations/20220126185722_nombredeposicion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BleylyMendez.Data.Migrations
+{
+    public partial class nombredeposicion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NomreDePosicion",
+                table: "Empleados",
+                newName: "NombreDePosicion");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NombreDePosicion",
+                table: "Empleados",
+                newName: "NomreDePosicion");
+        }
+    }
+}
