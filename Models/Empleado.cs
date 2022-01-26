542604c1f9cf3baee2c8b93f6db32bcc606bf69b
yosmerymendez22@hotmail.com <40107868+Edixon-vanVliet@users.noreply.github.com>
2022-01-26 10:55:33 -0400

    Agregar Models de Empleado

---
+++ Models/Empleado.cs
@@ -0,0 +1,13 @@
+namespace BleylyMendez.Models;
+
+public class Empleado
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = "";
+    public string Apellido { get; set; } = "";
+    public string Cedula { get; set; } = "";
+    public DateTime FechaDeNacimiento { get; set; }
+    public string NomreDePosicion { get; set; } = "";
+    public int DepartamentoId { get; set; }
+    public Departamento? Departamento { get; set; }
+}
