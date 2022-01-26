638122b8aa566baaf472b6a45783028d762c7f2e
yosmerymendez22@hotmail.com <40107868+Edixon-vanVliet@users.noreply.github.com>
2022-01-26 17:01:42 -0400

    Actualizar migracion a departamentoid Opcional

---
+++ Data/Migrations/20220126205938_DepartamentoIdOpcional.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BleylyMendez.Data.Migrations
+{
+    public partial class DepartamentoIdOpcional : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Empleados_Departamentos_DepartamentoId",
+                table: "Empleados");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentoId",
+                table: "Empleados",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Empleados_Departamentos_DepartamentoId",
+                table: "Empleados",
+                column: "DepartamentoId",
+                principalTable: "Departamentos",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Empleados_Departamentos_DepartamentoId",
+                table: "Empleados");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DepartamentoId",
+                table: "Empleados",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Empleados_Departamentos_DepartamentoId",
+                table: "Empleados",
+                column: "DepartamentoId",
+                principalTable: "Departamentos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
