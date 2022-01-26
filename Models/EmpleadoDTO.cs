58a65a4c27b93dca0f3a1a1e2a225583b9fd5d7f
yosmerymendez22@hotmail.com <40107868+Edixon-vanVliet@users.noreply.github.com>
2022-01-26 10:55:50 -0400

    Agregar Models de EmpleadoDTO

---
+++ Models/EmpleadoDTO.cs
@@ -0,0 +1,11 @@
+namespace BleylyMendez.Models;
+
+public class EmpleadoDTO
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = "";
+    public string Apellido { get; set; } = "";
+    public string Cedula { get; set; } = "";
+    public DateTime FechaDeNacimiento { get; set; }
+    public int DepartamentoId { get; set; }
+}
