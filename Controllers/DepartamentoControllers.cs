8ee08689996c9bb706284e54fc1fd85b55a6d18d
yosmerymendez22@hotmail.com <40107868+Edixon-vanVliet@users.noreply.github.com>
2022-01-26 18:07:02 -0400

    Agregando y ajustando listado de empleados

--- Controllers/DepartamentoControllers.cs
+++ Controllers/DepartamentoControllers.cs
@@ -1,6 +1,7 @@
 using BleylyMendez.Data;
 using BleylyMendez.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BleylyMendez.Controllers;
 
@@ -21,7 +22,7 @@ public class DepartamentosController : ControllerBase
     public ActionResult<List<Departamento>>
     GetDepartamento()
     {
-        return Ok(_context.Departamentos.ToList());
+        return Ok(_context.Departamentos.Include(departamento => departamento.Empleados).ToList());
     }
     //Get
     [HttpGet("{id}")]
@@ -38,8 +39,17 @@ public class DepartamentosController : ControllerBase
     //Post
     public ActionResult PostDepartamento([FromBody] Departamento departamento)
     {
+        foreach (var empleado in departamento.Empleados)
+        {
+            empleado.DepartamentoId = departamento.Id;
+            empleado.Departamento = departamento;
+        }
+
+        _context.Empleados.UpdateRange(departamento.Empleados);
+
         var departamentoCreated = _context.Departamentos.Add(departamento).Entity;
         _context.SaveChanges();
+
 
         return CreatedAtAction(nameof(GetDepartamento), new { id = departamentoCreated.Id }, departamentoCreated);
     }
@@ -49,7 +59,7 @@ public class DepartamentosController : ControllerBase
 
     public ActionResult PutDepartamento(int id, [FromBody] Departamento updatedDepartamento)
     {
-        var departamento = _context.Departamentos.Find(id);
+        var departamento = _context.Departamentos.Include(departamento => departamento.Empleados).FirstOrDefault(departamento => departamento.Id == id);
         if (departamento is null)
         {
             return NotFound();
@@ -57,7 +67,29 @@ public class DepartamentosController : ControllerBase
 
         departamento.Nombre = updatedDepartamento.Nombre;
         departamento.Descripcion = updatedDepartamento.Descripcion;
+        var empleados = departamento.Empleados.ToList();
 
+        empleados.ForEach(empleado =>
+        {
+            if (!updatedDepartamento.Empleados.Any(e => e.Id == empleado.Id))
+            {
+                empleado.DepartamentoId = null;
+                empleado.Departamento = null;
+            }
+        });
+
+        foreach (var empleado in updatedDepartamento.Empleados)
+        {
+            if (!empleados.Any(e => e.Id == empleado.Id))
+            {
+                empleado.DepartamentoId = id;
+                empleado.Departamento = departamento;
+
+                empleados.Add(empleado);
+            }
+        }
+
+        departamento.Empleados = empleados;
         _context.Departamentos.Update(departamento);
         _context.SaveChanges();
 
@@ -68,11 +100,18 @@ public class DepartamentosController : ControllerBase
     [HttpDelete("{id}")]
     public ActionResult DeleteDepartamento(int id)
     {
-        var departamento = _context.Departamentos.Find(id);
+        var departamento = _context.Departamentos.Include(departamento => departamento.Empleados).FirstOrDefault(departamento => departamento.Id == id);
         if (departamento is null)
         {
             return NotFound();
         }
+        foreach (var empleado in departamento.Empleados)
+        {
+            empleado.DepartamentoId = null;
+            empleado.Departamento = null;
+        }
+
+        _context.Empleados.UpdateRange(departamento.Empleados);
         _context.Departamentos.Remove(departamento);
         _context.SaveChanges();
 
--- Models/Departamento.cs
+++ Models/Departamento.cs
@@ -5,4 +5,6 @@ public class Departamento
     public int Id { get; set; }
     public string Nombre { get; set; } = "";
     public string Descripcion { get; set; } = "";
+
+    public IEnumerable<Empleado> Empleados { get; set; }
 }
